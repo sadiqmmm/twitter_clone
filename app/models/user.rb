db80f611fa2fd54e809d8e821c5347e714e69d45
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-07-07 00:36:26 +0530

    Finish User signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,6 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }               
   
   has_secure_password
-  validates :password, length: { minimum: 6 }  
+  validates :password,  length: { minimum: 6 } 
+
 end
