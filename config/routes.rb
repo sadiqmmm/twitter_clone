b481fd78411ad9fb9215d5a87b0cb9430b892bdf
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-07-07 17:22:46 +0530

    Add the microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
 
   match '/home', to: 'staticpages#home', via: :get
   match '/help', to: 'staticpages#help', via: :get
