c8a5857eab1c32eacf3358c0ebc58ec5fa8f76d9
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-07-07 16:52:27 +0530

    Finish user edit, update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def show  	 
@@ -39,6 +40,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user.destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
   end
 
   private
@@ -60,5 +64,9 @@ class UsersController < ApplicationController
     def correct_user
         @user = User.find(params[:id])
         redirect_to root_url, notice: "Unauthorize to edit user details" unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
