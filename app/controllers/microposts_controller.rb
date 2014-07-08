d9f53048535e6fb090d43cdff85cbe42e511fde2
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-07-08 12:58:58 +0530

    finish the micropost feed, delete

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
 	before_action :signed_in_user, only: [:create, :destroy]
+	before_action :correct_user, only: :destroy
 
  	def create
 		@micropost = current_user.microposts.build(micropost_params)
@@ -13,6 +14,8 @@ class MicropostsController < ApplicationController
 	end
 
 	def destroy
+		@micropost.destroy
+		redirect_to root_url
 	end
 
 	private
@@ -26,5 +29,10 @@ class MicropostsController < ApplicationController
         stored_location
         redirect_to signin_url, notice: "Please sign in" 
       end
+    end
+
+    def correct_user
+    	@micropost = current_user.microposts.find_by(id: params[:id])
+    	redirect_to root_url if @micropost.nil?
     end
 end
