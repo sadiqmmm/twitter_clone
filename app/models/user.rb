0ad64296c12d969ca389049fe3f6aa61e690847a
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-07-09 14:10:03 +0530

    Code formating

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
@@ -6,13 +7,17 @@ class User < ActiveRecord::Base
                                    class_name:  "Relationship",
                                    dependent:   :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
+  
   before_save { self.email = email.downcase }
   before_create :create_remember_token
+  
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  
   has_secure_password
+  
   validates :password, length: { minimum: 6 }
 
   def User.new_remember_token
