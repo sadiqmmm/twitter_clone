1d404222abd0cdadc11e81534e6e47eea463f77a
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-07-09 11:55:21 +0530

    finish the ajax working functionality and fix typo in relationship controller and add jquery turbolinks gem

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,24 +1,25 @@
 class RelationshipsController < ApplicationController
 	before_action :signed_in_user
 
-	def create
-		@user = User.find(params[:relationship][:followed_id])
-		current_user.follow!(@user)
-		redirect_to @user
-		respond_to do |format| 
-			format.html { redirect_to @user}
-			format.js
-		end
-	end
+	 def create
+       @user = User.find(params[:relationship][:followed_id])
+       current_user.follow!(@user)
+       
+       respond_to do |format|
+         format.html { redirect_to @user }
+         format.js
+      end
+  end
 
-	def destroy
-		@user = Relationship.find(params[:id]).followed
-		current_user.unfollow!(@user)
-		redirect_to @user
-		respond_to do |user|
-			format.html { redirect_to @user }
-			format.js 
-	end
+  def destroy
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow!(@user)
+    
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
 
 	def signed_in_user
       unless signed_in?        
