c269e7c5040cc64efb8c138140c8ab2bc31fc26a
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-07-09 14:06:52 +0530

    Fix a index in Relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,19 @@
 class User < ActiveRecord::Base
-
-  before_save { self.email = email.downcase}
-  before_create :create_remember_token
-  
   has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
-  has_many :reverse_relationships, foreign_key: "followed_id", 
-                                   class_name: "Relationship",
-                                   dependent: :destroy
-                                   
-  has_many :followers, through: :reverse_relationships, source: :follower
-                                   
   has_many :followed_users, through: :relationships, source: :followed
-  
-  validates :name, presence: true, length: { maximum: 50} 
-	
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
-                    uniqueness: { case_sensitive: false }               
-  
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password,  length: { minimum: 6 } 
+  validates :password, length: { minimum: 6 }
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
@@ -31,7 +24,7 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    Micropost.where("user_id=?", id)
+    Micropost.from_users_followed_by(self)
   end
 
   def following?(other_user)
@@ -45,9 +38,10 @@ class User < ActiveRecord::Base
   def unfollow!(other_user)
     relationships.find_by(followed_id: other_user.id).destroy
   end
-  
+
   private
+
     def create_remember_token
-    	self.remember_token = User.digest(User.new_remember_token)
+      self.remember_token = User.digest(User.new_remember_token)
     end
 end
